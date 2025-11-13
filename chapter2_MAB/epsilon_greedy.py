bb07c7bd8b4e62d0a4490a1d88644bed735a851e
Veinsure Lee <72303291+VeinsureLee@users.noreply.github.com>
2025-11-13 15:05:59 +0800

    Reorder print statements in epsilon_greedy.py

--- chapter2_MAB/epsilon_greedy.py
+++ chapter2_MAB/epsilon_greedy.py
@@ -123,6 +123,6 @@ print('epsilon 值衰减的贪婪算法的累积懊悔为：', decaying_epsilon_
 plot_results([decaying_epsilon_greedy_solver], ["DecayingEpsilonGreedy"])
 print('a')
 print('b')
-print('e')
 print('c')
 print('d')
+print('e')
